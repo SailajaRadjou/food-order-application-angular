af398af745869760fe9ecc4af99c0099066688ec
SailajaRadjou <p.sailaja@hotmail.com>
2022-06-26 00:00:53 +0200

    created components header, nav-bar, home & added images in the assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/services/food/food.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FoodService {
+
+  constructor() { }
+
+  getAll():string[]{
+    return [
+      '/assets/image1.jpg',
+      '/assets/image2.jpg',
+      '/assets/image3.jpg',
+      '/assets/image4.jpg',
+      '/assets/image5.jpg',
+      '/assets/image6.jpg',
+      '/assets/image7.jpg',
+      '/assets/image8.jpg',
+      '/assets/image9.jpg',
+      '/assets/image10.jpg',
+      '/assets/image11.jpg',
+      '/assets/image12.jpg',
+      '/assets/image13.jpg',
+      '/assets/image14.jpg',
+      '/assets/image15.jpg'
+    ]
+  }
+
+
+}
