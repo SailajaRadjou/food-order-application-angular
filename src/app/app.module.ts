d8c4b3ed867573e12da0e01159caf4eceb6c635d
SailajaRadjou <p.sailaja@hotmail.com>
2022-06-26 16:13:05 +0200

    added search components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,18 +9,21 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomeComponent } from './home/home.component';
 
 import { RatingModule } from 'ng-starrating';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     NavBarComponent,
-    HomeComponent
+    HomeComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RatingModule
+    RatingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { StarRatingComponent } from 'ng-starrating';
 import { FoodService } from '../services/food/food.service';
 import { Food } from '../models/food';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,10 +11,18 @@ import { Food } from '../models/food';
 export class HomeComponent implements OnInit {
 
   foods: Food[] = [];
-  constructor(private foodService: FoodService) { }
+  constructor(private foodService: FoodService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.foods = this.foodService.getAll();
+    this.route.params.subscribe(params => {
+      if (params['searchItem']) {
+        this.foods = this.foodService.getAll().filter(food => food.name.toLowerCase().includes(params['searchItem'].toLowerCase()));
+      } else {
+        this.foods = this.foodService.getAll();
+      }
+    })
+    
+    
   }
 
 }
